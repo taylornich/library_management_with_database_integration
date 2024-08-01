c811821dc9e01276391f0d95b3fc8f9e79df04ee
taylornich <166781921+taylornich@users.noreply.github.com>
2024-07-31 23:26:41 -0700

    Update connect_database.py
    Removing password

--- connect_database.py
+++ connect_database.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 def connect_db():
     db_name = "LibraryManagementSystem"
     user = "root"
-    password = "Brutus0309!"
+    password = ""
     host = "localhost"
 
     try: 
